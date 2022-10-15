7be54ffc827bfec91ccc308de037ea89f02a4282
Ngwu chukwudum moses <moseschukwudumsnr@gmail.com>
2022-10-14 18:15:29 -0700

    9-print_comb.c file.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-
- /**
- * main - Print all possible combinations of single digit numbers
+/**
+ * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(void)
 {
-	int num;
-	for (num = 0; num <= 9; num++)
+	int num;
+
+	for (num = 48; num <= 57; num++)
 	{
-		putchar(num + '0');
-		if (num < 9)
+		putchar(num);
+		if (num != 57)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(44);
+			putchar(32);
 		}
 	}
-	putchar('\n');
-	return (0);
+	putchar(10);
+	return (0);
 }
