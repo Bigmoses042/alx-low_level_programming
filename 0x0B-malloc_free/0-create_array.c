da129e0c576ded110da24080d74a4379e6d175c3
Ngwu chukwudum moses <moseschukwudumsnr@gmail.com>
2022-11-07 04:57:06 -0800

    creates an array.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,14 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	int i = 0;
 
-	if (size <= 0) 
+	if (size <= 0) /* validate size input */
 		return (NULL);
-	arr = malloc(sizeof(char) * size); 
+	arr = malloc(sizeof(char) * size); /* allocate memory */
 
-	if (arr == NULL) 
+	if (arr == NULL) /* validate memory */
 		return (NULL);
 
-	while (i < (int)size) 
+	while (i < (int)size) /* set array values to char c */
 	{
 		*(arr + i) = c;
 		i++;
