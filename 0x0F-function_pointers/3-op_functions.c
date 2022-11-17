93c039267e4f8239b41f454b24babd2e4376fe21
Ngwu chukwudum moses <moseschukwudumsnr@gmail.com>
2022-11-16 18:50:45 -0800

    is alright.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,63 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * op_add - addition
+ * @a: number 1
+ * @b: number 2
+ * Return: the addend of the two numbers
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * op_sub - Substraction
+ * @a: number 1
+ * @b: number 2
+ * Return: the substraction of the two numbers
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * op_mul - multiplication
+ * @a: number 1
+ * @b: number 2
+ * Return: the multiplication of the two numbers
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * op_div - division
+ * @a: number 1
+ * @b: number 2
+ * Return: the questiont of the two numbers
  */
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
 }
+
 /**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * op_mod - modules
+ * @a: number 1
+ * @b: number 2
+ * Return: the modules of the two numbers
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b != 0)
+		return (a % b);
+	printf("Error\n");
+	exit(100);
 }
