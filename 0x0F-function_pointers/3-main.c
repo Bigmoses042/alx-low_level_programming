13f2e085cf99e7dcc9129ada84221c4caf3f2256
Ngwu chukwudum moses <moseschukwudumsnr@gmail.com>
2022-11-18 15:02:13 -0800

    we add the main.v.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,42 @@
-#ifndef CALC_H
-#define CALC_H
-
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct op - Struct op
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * @op: The operator
- * @f: The function associated
+ * Return: Always 0.
  */
-typedef struct op
+int main(int argc, char *argv[])
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	int a, b;
+	int (*operation)(int, int);
 
-#endif
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
